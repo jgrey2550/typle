0f2ca33508e71f5c7c558950a892b8ff077b2657
jgrey2550 <jgrey2550@gmail.com>
2023-07-24 15:15:18 -0700

    first

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 5000;
+//port from .env file
+
+app.use(cors());
+// app.use(cors(
+//     {
+//         origin: ["https://typle2-frontend.vercel.app"],
+//         methods: ["POST", "GET", "PUT"],
+//         credentials: true
+//     }
+// ));
+
+const uri = 'mongodb+srv://jgrey2550:Rq5GF6hwVRYtuutF@cluster0.qgvh47s.mongodb.net/?retryWrites=true&w=majority';
+//mongodb uri from .env file
+
+mongoose.connect('mongodb+srv://jgrey2550:Rq5GF6hwVRYtuutF@cluster0.qgvh47s.mongodb.net/?retryWrites=true&w=majority');
+// mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.once('open', () => {
+    console.log("Connected MONGODB");
+})
+//connect to mongodb
+
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
+
+const createUserRoute = require('./routes/createUser');
+const loginRoute = require('./routes/login');
+const globalUser = require('./routes/globalUser');
+//routes for login an createUser https
+
+app.use('/createUser', createUserRoute);
+app.use('/login', loginRoute);
+app.use('/api', globalUser);
+// /api for global route can be accessed anywhere
+
+app.listen(port, () => {
+    console.log('server run port 5000');
+});
+//run backend on port 5000
