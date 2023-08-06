aa3454c7ea2a2dd9a4038c55b89306181c6e6896
jgrey2550 <jgrey2550@gmail.com>
2023-08-06 01:13:11 -0700

    oauth

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,9 +1,10 @@
-import React, {useContext} from "react";
+import React, {useContext, useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { UserContext } from "../contexts/userContext";
 import { useNavigate } from "react-router-dom";
 import { ApiContext } from "../contexts/apiContext";
+import jwt_decode from "jwt-decode";
 
 function Login() {
     const {apiUrl} = useContext(ApiContext);
@@ -53,6 +54,52 @@ function Login() {
         //post request to /login/ with axios to scan mongodb for matching username + password
     }
 
+    const [userStuff, setUserStuff] = useState({});
+
+    function handleCallbackResponse(response) {
+        console.log("encoded id token" + response.credential);
+        var userObject = jwt_decode(response.credential);
+        console.log(userObject);
+        setUserStuff(userObject);
+        // document.getElementById("signInDiv").hidden = true;
+        const loginData = {
+            username: userObject.email,
+            password: userObject.name
+        }
+
+        console.log(loginData);
+
+        //post request to check for user credentials
+        axios.post(`${apiUrl}/login/`, loginData)
+        .then(res => {
+            if (res.data.login === 'Login successful') {
+                const userId = res.data.userId;
+                //updates user with userID that was passed back from the post requeste
+                updateUser(userId);
+                navigate('/menu');
+            } else {
+                alert('No Account linked to this email');
+            }
+        })
+        .catch(error => console.log(error));
+
+        onChangePassword('');
+        onChangeUsername('');
+    }
+
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+        client_id: "556849085560-osvh0sm4f0rf152a9a20tf85bfd3vkhv.apps.googleusercontent.com",
+        callback: handleCallbackResponse
+        });
+
+        google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        {theme: "outline", size: "large"}
+        );
+    }, []);
+
     return <div>
         <h1>Login below!</h1>
         <form onSubmit={onSubmit}>
@@ -77,6 +124,9 @@ function Login() {
                 <div>
                     <button type="submit">Submit</button>
                 </div>
+                <br/>
+                <h3>Login With Google</h3>
+                <div id="signInDiv"></div>
             </form>
         <Link to="/createUser">Create Account</Link>
     </div>
